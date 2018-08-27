af8b6d9b5f1684688bdb465d2772723e7f70ce66
Aiden Wallis <aiden@aidenwallis.co.uk>
2018-08-27 18:34:57 +0100

    clean up usernotices

--- src/app/templates/chat/usernotice.js
+++ src/app/templates/chat/usernotice.js
@@ -1,7 +1,7 @@
 import escape from 'lodash/escape';
 
 const displayNameTemplate = (name, login) => `<a href="https://twitch.tv/${escape(login)}" target="_blank" rel="noreferrer noopener" class="chat-line-usernotice-name">${escape(name)}</a>`;
-const primePlugTemplate = (channel) => `<a href="https://twitch.amazon.com/prime?channel=%23${escape(channel)}&ref=subscriptionMessage" target="_blank" rel-"noopener noreferrer">Twitch Prime</a>`;
+const primePlugTemplate = (channel) => `<a href="https://twitch.amazon.com/prime?channel=%23${escape(channel)}&ref=subscriptionMessage" target="_blank" rel-"noopener noreferrer" class="chat-line-usernotice-prime">Twitch Prime</a>`;
 
 const parseSystemMsg = (message) => {
   let systemMsg = message.tags['system-msg'];
